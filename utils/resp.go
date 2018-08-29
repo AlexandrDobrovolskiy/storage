2e6332eba35ebc3de6d71bcab0eb106375d5e352
Alexandr <sasha732sasha000@gmail.com>
2018-08-29 22:49:15 +0300

    cors

--- utils/resp.go
+++ utils/resp.go
@@ -11,5 +11,9 @@ func Message(status bool, message string) map[string]interface{} {
 
 func Respond(w http.ResponseWriter, data map[string]interface{}) {
 	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Add("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time")
+	w.Header().Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
 	json.NewEncoder(w).Encode(data)
 }
